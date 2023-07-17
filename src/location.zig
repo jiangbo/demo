0c4903b8826eae7851cd1332d6559ba307e26f88
jiangbo <1006758852@qq.com>
2023-07-17 22:35:48 +0800

    提交游戏

--- src/location.zig
+++ src/location.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const world = @import("world.zig");
 const getVisible = @import("noun.zig").getVisible;
-const misc = @import("misc.zig");
 const print = std.debug.print;
 
 pub fn executeLook(input: ?[]const u8) bool {
@@ -22,26 +21,27 @@ pub fn lookAround() bool {
 pub fn executeGo(input: ?[]const u8) bool {
     const noun = input orelse return false;
 
-    var obj = getVisible("where you want to go", noun) orelse return true;
-    if (obj.location == null and obj != object.Entity.Player.location.?) {
+    var item = world.getItem(noun) orelse return false;
+    if (item.isLocation() and item.isPlayerNotIn()) {
         print("OK.\n", .{});
-        object.Entity.Player.location = obj;
+        world.player.location = item;
         return lookAround();
     } else {
         print("You can't get much closer than this.\n", .{});
+        return true;
     }
 
     return false;
 }
 
-fn listAtLocation(location: *world.Item) void {
+fn listAtLocation(location: *const world.Item) void {
     var count: i32 = 0;
-    for (&world.items) |item| {
-        if (item.isNotPlayer() and location.isNotSelf(item)) {
+    for (world.items) |item| {
+        if (item.isNotPlayer() and item.isLocate(location)) {
             if (count == 0) {
                 print("You see:\n", .{});
             }
-            print("{s}\n", .{location.desc});
+            print("{s}\n", .{item.desc});
             count += 1;
         }
     }
--- src/world.zig
+++ src/world.zig
@@ -1,35 +1,52 @@
-const Type = enum { Field, Cave, Silver, Gold, Guard, Player };
+const std = @import("std");
+const print = std.debug.print;
+
+const Type = enum { field, cave, silver, gold, guard, player };
 
 pub const Item = struct {
     desc: []const u8,
     type: Type,
-    location: ?*Item = null,
+    location: ?*const Item = null,
 
-    pub fn isNotPlayer(self: *Item) bool {
-        return self.type != .Player;
+    pub fn isNotPlayer(self: *const Item) bool {
+        return self.type != .player;
     }
 
-    pub fn isLocation(self: *Item) bool {
-        return self.type == .Field or self.type == .Cave;
+    pub fn isLocation(self: *const Item) bool {
+        return self.type == .field or self.type == .cave;
     }
 
-    pub fn isNotSelf(self: *Item, item: *Item) bool {
-        return self == item;
+    pub fn isLocate(self: *const Item, location: *const Item) bool {
+        return self.location == location;
     }
 
-    pub fn toEnum() void {
-        @stringToEnum();
+    pub fn isPlayerNotIn(self: *const Item) bool {
+        return self != player.location.?;
     }
 };
 
 pub var items = [_]Item{
-    .{ .desc = "an open field", .type = .Field },
-    .{ .desc = "a little cave", .type = .Cave },
-    .{ .desc = "a silver coin", .type = .Silver },
-    .{ .desc = "a gold coin", .type = .Gold },
-    .{ .desc = "a burly guard", .type = .Guard },
-    .{ .desc = "an open field", .type = .Player },
+    .{ .desc = "an open field", .type = .field },
+    .{ .desc = "a little cave", .type = .cave },
+    .{ .desc = "a silver coin", .type = .silver },
+    .{ .desc = "a gold coin", .type = .gold },
+    .{ .desc = "a burly guard", .type = .guard },
+    .{ .desc = "yourself", .type = .player },
 };
+
+fn toType(noun: []const u8) ?Type {
+    return std.meta.stringToEnum(Type, noun);
+}
+
+pub fn getItem(noun: []const u8) ?*const Item {
+    const itemType = toType(noun) orelse return null;
+    for (items) |*value| {
+        if (value.type == itemType) {
+            return value;
+        }
+    }
+    return null;
+}
 
 pub var player: *Item = &items[5];
 
