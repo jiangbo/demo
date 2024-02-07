cd064246987c90ab9bf516ca4776a9e48c85c52c
JiangBo <1006758852@qq.com>
2024-02-07 15:52:54 +0800

    提交游戏

--- src/main.zig
+++ src/main.zig
@@ -1,96 +1,108 @@
 const std = @import("std");
 const ray = @import("raylib.zig");
-const map = @import("map.zig");
+
+const screenWidth = 800;
+const screenHeight = 450;
+
+const MAX_BUILDINGS = 100;
+
+var player: ray.Rectangle = .{ .x = 400, .y = 280, .width = 40, .height = 40 };
+var buildings: [MAX_BUILDINGS]ray.Rectangle = undefined;
+var buildColors: [MAX_BUILDINGS]ray.Color = undefined;
 
 pub fn main() void {
+    ray.InitWindow(screenWidth, screenHeight, "raylib [core] example");
+    defer ray.CloseWindow();
+    ray.SetTargetFPS(60);
 
-    // 初始化地图
-    var state: [map.stageLength]map.MapItem = undefined;
-    map.init(&state);
-    defer map.deinit();
+    var spacing: f32 = 0;
 
-    const stdin = std.io.getStdIn().reader();
+    for (0..MAX_BUILDINGS) |index| {
+        buildings[index].width = @floatFromInt(ray.GetRandomValue(50, 200));
+        buildings[index].height = @floatFromInt(ray.GetRandomValue(100, 800));
+        buildings[index].y = screenHeight - 130.0 - buildings[index].height;
+        buildings[index].x = -6000.0 + spacing;
 
-    while (true) {
+        spacing += buildings[index].width;
 
-        // 画出游戏地图
-        ray.BeginDrawing();
-        ray.ClearBackground(ray.WHITE);
-
-        map.draw(&state);
-
-        ray.EndDrawing();
-
-        // 检查游戏胜利条件
-        if (checkClear(&state)) break;
-
-        std.debug.print("a:left d:right w:up s:down. command?\n", .{});
-        // 获取用户输入
-        const char = inputChar(stdin);
-        // 根据输入更新游戏地图
-        update(&state, char);
+        buildColors[index] = ray.Color{
+            .r = @intCast(ray.GetRandomValue(200, 240)),
+            .g = @intCast(ray.GetRandomValue(200, 240)),
+            .b = @intCast(ray.GetRandomValue(200, 250)),
+            .a = 255,
+        };
     }
 
-    // 游戏胜利
-    std.debug.print("Congratulation's! you win.\n", .{});
-}
-
-fn checkClear(stage: []map.MapItem) bool {
-    for (stage) |value| {
-        if (value == map.MapItem.BLOCK) {
-            return false;
-        }
-    }
-    return true;
-}
-
-fn inputChar(reader: anytype) ?u8 {
-    var buffer: [2]u8 = undefined;
-    const input = reader.readUntilDelimiterOrEof(buffer[0..], '\n') //
-    catch null orelse return null;
-    return if (input.len != 1) null else input[0];
-}
-
-fn update(state: []map.MapItem, input: ?u8) void {
-    const char = input orelse return;
-
-    // 操作角色移动的距离
-    const delta: isize = switch (char) {
-        'w' => -map.stageWidth,
-        's' => map.stageWidth,
-        'd' => 1,
-        'a' => -1,
-        else => return,
+    var camera: ray.Camera2D = .{
+        .target = .{ .x = player.x + 20.0, .y = player.y + 20.0 },
+        .offset = .{ screenWidth / 2.0, screenHeight / 2.0 },
+        .rotation = 0.0,
+        .zoom = 1.0,
     };
 
-    // 角色当前位置
-    const currentIndex = for (state, 0..) |value, index| {
-        if (value == .MAN or value == .MAN_ON_GOAL) break index;
-    } else return;
+    while (!ray.WindowShouldClose()) {
+        update(&player, &camera);
 
-    const index = @as(isize, @intCast(currentIndex)) + delta;
-    if (index < 0 or index > map.stageLength) return;
-
-    // 角色欲前往的目的地
-    const destIndex = @as(usize, @intCast(index));
-    updatePlayer(state, currentIndex, destIndex, delta);
+        ray.BeginDrawing();
+        defer ray.EndDrawing();
+        draw();
+    }
 }
 
-fn updatePlayer(state: []map.MapItem, current: usize, dest: usize, delta: isize) void {
-    if (state[dest] == .SPACE or state[dest] == .GOAL) {
-        // 如果是空地或者目标地，则可以移动
-        state[dest] = if (state[dest] == .GOAL) .MAN_ON_GOAL else .MAN;
-        state[current] = if (state[current] == .MAN_ON_GOAL) .GOAL else .SPACE;
-    } else if (state[dest] == .BLOCK or state[dest] == .BLOCK_ON_GOAL) {
-        //  如果是箱子或者目的地上的箱子，需要考虑该方向上的第二个位置
-        const index = @as(isize, @intCast(dest)) + delta;
-        if (index < 0 or index > map.stageLength) return;
+fn update(camera: *ray.Camera2D) void {
+    if (ray.IsKeyDown(ray.KEY_RIGHT)) player.x += 2 else if (ray.IsKeyDown(ray.KEY_LEFT)) player.x -= 2;
 
-        const next = @as(usize, @intCast(index));
-        if (state[next] == .SPACE or state[next] == .GOAL) {
-            state[next] = if (state[next] == .GOAL) .BLOCK_ON_GOAL else .BLOCK;
-            state[dest] = if (state[dest] == .BLOCK_ON_GOAL) .MAN_ON_GOAL else .MAN;
-            state[current] = if (state[current] == .MAN_ON_GOAL) .GOAL else .SPACE;
-        }
+    // Camera target follows player
+    camera.target = .{ player.x + 20, player.y + 20 };
+
+    // Camera rotation controls
+    if (ray.IsKeyDown(ray.KEY_A)) camera.rotation -= 1 else if (ray.IsKeyDown(ray.KEY_S)) camera.rotation += 1;
+
+    // Limit camera rotation to 80 degrees (-40 to 40)
+    if (camera.rotation > 40) camera.rotation = 40 else if (camera.rotation < -40) camera.rotation = -40;
+
+    // Camera zoom controls
+    camera.zoom += (ray.GetMouseWheelMove() * 0.05);
+
+    if (camera.zoom > 3.0) camera.zoom = 3.0 else if (camera.zoom < 0.1) camera.zoom = 0.1;
+
+    // Camera reset (zoom and rotation)
+    if (ray.IsKeyPressed(ray.KEY_R)) {
+        camera.zoom = 1.0;
+        camera.rotation = 0.0;
     }
+}
+
+fn draw(camera: *ray.Camera2D) void {
+    ray.ClearBackground(ray.RAYWHITE);
+
+    ray.BeginMode2D(camera);
+
+    ray.DrawRectangle(-6000, 320, 13000, 8000, ray.DARKGRAY);
+
+    for (0..MAX_BUILDINGS) |index|
+        ray.DrawRectangleRec(buildings[index], buildColors[index]);
+
+    ray.DrawRectangleRec(player, ray.RED);
+
+    ray.DrawLine(camera.target.x, -screenHeight * 10, camera.target.x, screenHeight * 10, ray.GREEN);
+    ray.DrawLine(-screenWidth * 10, camera.target.y, screenWidth * 10, camera.target.y, ray.GREEN);
+
+    ray.EndMode2D();
+
+    ray.DrawText("SCREEN AREA", 640, 10, 20, ray.RED);
+
+    ray.DrawRectangle(0, 0, screenWidth, 5, ray.RED);
+    ray.DrawRectangle(0, 5, 5, screenHeight - 10, ray.RED);
+    ray.DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, ray.RED);
+    ray.DrawRectangle(0, screenHeight - 5, screenWidth, 5, ray.RED);
+
+    ray.DrawRectangle(10, 10, 250, 113, ray.Fade(ray.SKYBLUE, 0.5));
+    ray.DrawRectangleLines(10, 10, 250, 113, ray.BLUE);
+
+    ray.DrawText("Free 2d camera controls:", 20, 20, 10, ray.BLACK);
+    ray.DrawText("- Right/Left to move Offset", 40, 40, 10, ray.DARKGRAY);
+    ray.DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, ray.DARKGRAY);
+    ray.DrawText("- A / S to Rotate", 40, 80, 10, ray.DARKGRAY);
+    ray.DrawText("- R to reset Zoom and Rotation", 40, 100, 10, ray.DARKGRAY);
 }
