72693ea247db9b3dad02304121e0d8f16e3d52f8
JiangBo <1006758852@qq.com>
2024-03-29 00:30:30 +0800

    提交游戏

--- src/main.zig
+++ src/main.zig
@@ -11,18 +11,15 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     const number: u16 = @as(u16, rule[0]) << 8 | rule[1];
-    std.log.debug("number: {}", .{number});
+    std.log.debug("rulenumber: {}", .{number});
 
     const rules = try allocator.alloc(Rule, number);
     defer allocator.free(rules);
     for (0..number) |value| {
         const index = 2 + (value * 6);
-        const a = rule[index .. index + 4];
-        const b = rule[index + 4 .. index + 6];
-        rules[value] = .{
-            .a = bigToNative(u32, @as(*align(1) const u32, @ptrCast(a)).*),
-            .b = bigToNative(u16, @as(*align(1) const u16, @ptrCast(b)).*),
-        };
+        const a = rule[index..];
+        const b = rule[index + 4 ..];
+        rules[value] = .{ .a = sliceInt(a, u32), .b = sliceInt(b, u16) };
     }
 
     for (rules) |value| {
@@ -34,3 +31,8 @@ const Rule = struct {
     a: u32,
     b: u16,
 };
+
+fn sliceInt(slice: []const u8, comptime T: type) T {
+    const ptr: *align(1) const T = @ptrCast(slice[0..@sizeOf(T)]);
+    return std.mem.bigToNative(T, ptr.*);
+}
