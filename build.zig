5a4ed73efe9c6a3a1a908570d8e2d404dbe1e0b5
JiangBo <1006758852@qq.com>
2024-05-08 14:56:05 +0800

    提交游戏

--- build.zig
+++ build.zig
@@ -1,37 +1,35 @@
 const std = @import("std");
+const mach = @import("mach");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const mach_dep = b.dependency("mach", .{
+        .target = target,
+        .optimize = optimize,
+        .core = true,
+    });
+    const app = try mach.CoreApp.init(b, mach_dep.builder, .{
         .name = "demo",
+        .src = "src/main.zig",
+        .target = target,
+        .optimize = optimize,
+        .deps = &[_]std.Build.Module.Import{},
+    });
+
+    if (b.args) |args| app.run.addArgs(args);
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&app.run.step);
+
+    const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    const glfw_dep = b.dependency("mach-glfw", .{ .target = target, .optimize = optimize });
-    exe.root_module.addImport("mach-glfw", glfw_dep.module("mach-glfw"));
-
-    const zlm = b.dependency("zlm", .{});
-    exe.root_module.addImport("zlm", zlm.module("zlm"));
-
-    const options = .{ .api = .gl, .version = .@"3.3", .profile = .core };
-    const gl_bindings = @import("zigglgen").generateBindingsModule(b, options);
-    exe.root_module.addImport("gl", gl_bindings);
-
-    const zstbi = b.dependency("zstbi", .{ .target = target, .optimize = optimize });
-    exe.root_module.addImport("zstbi", zstbi.module("root"));
-    exe.linkLibrary(zstbi.artifact("zstbi"));
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| run_cmd.addArgs(args);
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,86 +1,49 @@
 const std = @import("std");
-const zlm = @import("zlm");
-const glfw = @import("mach-glfw");
-const gl = @import("gl");
-const resource = @import("resource.zig");
-const zstbi = @import("zstbi");
-const Game = @import("game.zig").Game;
+const mach = @import("mach");
 
-fn logGlfwError(code: glfw.ErrorCode, description: [:0]const u8) void {
-    std.log.err("{}: {s}\n", .{ code, description });
+pub const App = @This();
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+pub fn init(_: *App) !void {
+    try mach.core.init(.{
+        .size = .{ .width = 800, .height = 600 },
+        .title = "学习 WebGPU",
+    });
 }
 
-fn glfwPanic() noreturn {
-    @panic(glfw.getErrorString() orelse "unknown error");
-}
-
-var breakout: Game = Game{ .width = 800, .height = 600 };
-var glProcs: gl.ProcTable = undefined;
-
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+pub fn deinit(_: *App) void {
     defer _ = gpa.deinit();
-
-    glfw.setErrorCallback(logGlfwError);
-    if (!glfw.init(.{})) glfwPanic();
-    defer glfw.terminate();
-    const window = glfw.Window.create(800, 600, "学习 OpenGL", null, null, .{
-        .context_version_major = gl.info.version_major,
-        .context_version_minor = gl.info.version_minor,
-        .opengl_profile = .opengl_core_profile,
-    }) orelse glfwPanic();
-    defer window.destroy();
-
-    glfw.makeContextCurrent(window);
-    defer glfw.makeContextCurrent(null);
-    glfw.swapInterval(1);
-    glfw.Window.setFramebufferSizeCallback(window, windowChange);
-    glfw.Window.setKeyCallback(window, keyCallback);
-
-    if (!glProcs.init(glfw.getProcAddress)) glfwPanic();
-
-    gl.makeProcTableCurrent(&glProcs);
-    defer gl.makeProcTableCurrent(null);
-    gl.Enable(gl.BLEND);
-    gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-
-    try breakout.init(gpa.allocator());
-    defer breakout.deinit();
-    var lastFrame: f32 = 0.0;
-
-    while (!window.shouldClose()) {
-        const currentFrame = @as(f32, @floatCast(glfw.getTime()));
-        defer lastFrame = currentFrame;
-        const deltaTime = currentFrame - lastFrame;
-
-        glfw.pollEvents();
-        breakout.processInput(deltaTime);
-        breakout.update(deltaTime);
-
-        gl.ClearColor(0, 0, 0, 0);
-        gl.Clear(gl.COLOR_BUFFER_BIT);
-
-        breakout.render();
-        window.swapBuffers();
-    }
+    defer mach.core.deinit();
 }
 
-fn windowChange(_: glfw.Window, w: u32, h: u32) void {
-    gl.Viewport(0, 0, @as(c_int, @intCast(w)), @as(c_int, @intCast(h)));
-}
+pub fn update(_: *App) !bool {
+    var iterator = mach.core.pollEvents();
+    while (iterator.next()) |event| if (event == .close) return true;
 
-fn keyCallback(
-    window: glfw.Window,
-    key: glfw.Key,
-    scancode: i32,
-    action: glfw.Action,
-    _: glfw.Mods,
-) void {
-    if (key == .escape and action == .press)
-        window.setShouldClose(true);
-    if (scancode >= 0 and scancode < 1024) {
-        const index: usize = @intCast(scancode);
-        if (action == .press) breakout.keys[index] = true //
-        else if (action == .release) breakout.keys[index] = false;
-    }
+    const view = mach.core.swap_chain.getCurrentTextureView().?;
+    const colorAttachment = mach.gpu.RenderPassColorAttachment{
+        .view = view,
+        .clear_value = mach.gpu.Color{ .r = 0, .g = 0, .b = 0, .a = 1.0 },
+        .load_op = .clear,
+        .store_op = .store,
+    };
+
+    const encoder = mach.core.device.createCommandEncoder(null);
+    const renderPass = mach.gpu.RenderPassDescriptor.init(.{
+        .color_attachments = &.{colorAttachment},
+    });
+
+    const pass = encoder.beginRenderPass(&renderPass);
+    pass.end();
+    pass.release();
+
+    var command = encoder.finish(null);
+    encoder.release();
+
+    var queue = mach.core.queue;
+    queue.submit(&[_]*mach.gpu.CommandBuffer{command});
+    command.release();
+    mach.core.swap_chain.present();
+    view.release();
+    return false;
 }
