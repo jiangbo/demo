304ce5305bba8ba3e82d776bc5509376a8e6dda3
JiangBo <1006758852@qq.com>
2024-05-10 18:06:00 +0800

    提交游戏

--- src/main.zig
+++ src/main.zig
@@ -5,30 +5,37 @@ pub const App = @This();
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 renderPipeline: *mach.gpu.RenderPipeline,
-timer: mach.core.Timer,
 
 pub fn init(app: *App) !void {
+
+    // 定义了窗口的宽和高，以及窗口的标题
     try mach.core.init(.{
         .size = .{ .width = 800, .height = 600 },
         .title = "学习 WebGPU",
     });
 
+    // 设置帧率
+    mach.core.setFrameRateLimit(30);
+    mach.core.setInputFrequency(30);
     const device = mach.core.device;
+
+    // 编译 shader
     const source = @embedFile("shader/shader.wgsl");
     const shader = device.createShaderModuleWGSL("shader.wgsl", source);
     defer shader.release();
 
+    // 片段着色器
     const fragment = mach.gpu.FragmentState.init(.{
         .module = shader,
-        .entry_point = "fragment_main",
+        .entry_point = "fs_main",
         .targets = &.{.{ .format = mach.core.descriptor.format }},
     });
 
+    // 创建渲染管线
     app.renderPipeline = device.createRenderPipeline(&.{
-        .vertex = .{ .module = shader, .entry_point = "vertex_main" },
+        .vertex = .{ .module = shader, .entry_point = "vs_main" },
         .fragment = &fragment,
     });
-    app.timer = try mach.core.Timer.start();
 }
 
 pub fn deinit(app: *App) void {
@@ -38,9 +45,12 @@ pub fn deinit(app: *App) void {
 }
 
 pub fn update(app: *App) !bool {
+
+    // 检查窗口是否需要关闭
     var iterator = mach.core.pollEvents();
     while (iterator.next()) |event| if (event == .close) return true;
 
+    // 清屏使用
     const view = mach.core.swap_chain.getCurrentTextureView().?;
     const colorAttachment = mach.gpu.RenderPassColorAttachment{
         .view = view,
@@ -53,29 +63,25 @@ pub fn update(app: *App) !bool {
         .color_attachments = &.{colorAttachment},
     });
 
+    // 命令编码器
     const encoder = mach.core.device.createCommandEncoder(null);
     const pass = encoder.beginRenderPass(&renderPass);
+    // 绘制
     pass.setPipeline(app.renderPipeline);
-    pass.draw(3, 1, 0, 0);
+    // 六个点，画两个三角形
+    pass.draw(6, 2, 0, 0);
     pass.end();
     pass.release();
 
     var command = encoder.finish(null);
     encoder.release();
 
+    // 提交命令
     mach.core.queue.submit(&.{command});
     command.release();
     mach.core.swap_chain.present();
     view.release();
 
-    // update the window title every second
-    if (app.timer.read() >= 1.0) {
-        app.timer.reset();
-        try mach.core.printTitle("[ {d}fps ] [ Input {d}hz ]", .{
-            mach.core.frameRate(),
-            mach.core.inputRate(),
-        });
-    }
-
+    // 不退出渲染循环
     return false;
 }
--- src/shader/shader.wgsl
+++ src/shader/shader.wgsl
@@ -1,14 +1,25 @@
+struct PosAndColor {
+    @builtin(position) pos : vec4f,
+    @location(0) color : vec4f
+};
+
 @vertex
-fn vertex_main(@builtin(vertex_index) VertexIndex : u32) -> @builtin(position) vec4<f32> {
-    var pos = array<vec2<f32>, 3>(
-        vec2<f32>( 0.0,  0.5),
-        vec2<f32>(-0.5, -0.5),
-        vec2<f32>( 0.5, -0.5)
+fn vs_main(@builtin(vertex_index) VertexIndex : u32) -> PosAndColor {
+    let pos = array(
+        vec2f( 0.5,  0.5),
+        vec2f( 0.5, -0.5),
+        vec2f(-0.5, -0.5),
+        vec2f(-0.5, -0.5),
+        vec2f(-0.5,  0.5),
+        vec2f( 0.5,  0.5)
     );
-    return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+
+    let pos4f = vec4f(pos[VertexIndex], 0.0, 1.0);
+
+    return PosAndColor(pos4f, vec4f(0.9, 0.5, 0.7, 1.0));
 }
 
 @fragment
-fn fragment_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+fn fs_main(in: PosAndColor) -> @location(0) vec4f {
+    return in.color;
 }
