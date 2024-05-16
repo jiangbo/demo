de1907b829281434b257b3f4ff9470aed135a46e
jiangbo <1006758852@qq.com>
2024-05-16 21:48:14 +0800

    提交游戏

--- src/main.zig
+++ src/main.zig
@@ -5,9 +5,10 @@ const mach = @import("mach");
 pub const App = @This();
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-var renderPipeline: mach.gpu.RenderPipeline = undefined;
+renderPipeline: *mach.gpu.RenderPipeline = undefined,
+bindGroup: *mach.gpu.BindGroup = undefined,
 
-pub fn init(_: *App) !void {
+pub fn init(app: *App) !void {
     try mach.core.init(.{});
     // 设置帧率
     mach.core.setFrameRateLimit(30);
@@ -15,9 +16,9 @@ pub fn init(_: *App) !void {
 
     const device = mach.core.device;
 
-    const red = 0xFF0000FF;
-    const yellow = 0xFFFF00FF;
-    const blue = 0x0000FFFF;
+    const red: u32 = 0xFF0000FF;
+    const yellow: u32 = 0xFFFF00FF;
+    const blue: u32 = 0x0000FFFF;
 
     const textureData = [_]u32{
         blue, red,    red,    red,    red, //
@@ -30,85 +31,28 @@ pub fn init(_: *App) !void {
     };
 
     const size = mach.gpu.Extent3D{ .width = 5, .height = 7 };
-    const texture = device.createTexture(.{
+    const texture = device.createTexture(&.{
         .label = "F texutre",
         .size = .{ .width = size.width, .height = size.height },
         .format = .rgba8_unorm,
-        .usage = .{ .texture_binding = true, .copy_dst = true },
+        .usage = .{ .texture_binding = true, .copy_dst = true, .render_attachment = true },
     });
 
-    device.getQueue().writeTexture(&texture, &.{
+    device.getQueue().writeTexture(&.{ .texture = texture }, &.{
         .bytes_per_row = @sizeOf(u32) * 5,
+        .rows_per_image = 7,
     }, &size, &textureData);
 
-    const sampler = device.createSampler();
-
-    const bindGroup = device.createBindGroup({
-    layout: pipeline.getBindGroupLayout(0),
-    entries: [
-      { binding: 0, resource: sampler },
-      { binding: 1, resource: texture.createView() },
-    ],
-  });
-
-  const renderPassDescriptor = {
-    label: 'our basic canvas renderPass',
-    colorAttachments: [
-      {
-        // view: <- to be filled out when we render
-        clearValue: [0.3, 0.3, 0.3, 1],
-        loadOp: 'clear',
-        storeOp: 'store',
-      },
-    ],
-  };
-
-    // 输出 buffer
-    const resultBuffer = device.createBuffer(&.{
-        .label = "result buffer",
-        .size = @sizeOf(@TypeOf(input)),
-        .usage = .{ .map_read = true, .copy_dst = true },
+    const sampler = device.createSampler(null);
+    app.renderPipeline = createRenderPipeline();
+    app.bindGroup = device.createBindGroup(&.{
+        .layout = app.renderPipeline.getBindGroupLayout(0),
+        .entry_count = 2,
+        .entries = (&[2]mach.gpu.BindGroup.Entry{
+            mach.gpu.BindGroup.Entry.sampler(0, sampler),
+            mach.gpu.BindGroup.Entry.textureView(1, texture.createView(null)),
+        }),
     });
-    defer resultBuffer.release();
-
-    // 提交指令
-    const encoder = device.createCommandEncoder(null);
-    const pass = encoder.beginComputePass(null);
-
-    pass.setPipeline(re);
-    pass.setBindGroup(0, bindGroup, &.{});
-    pass.dispatchWorkgroups(input.len, 1, 1);
-    pass.end();
-    pass.release();
-
-    encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, size);
-
-    var commandBuffer = encoder.finish(null);
-    encoder.release();
-
-    mach.core.queue.submit(&[_]*mach.gpu.CommandBuffer{commandBuffer});
-    commandBuffer.release();
-
-    // 异步得到返回结果
-    const Status = mach.gpu.Buffer.MapAsyncStatus;
-    var response: Status = undefined;
-    resultBuffer.mapAsync(.{ .read = true }, 0, size, &response, struct {
-        pub inline fn callback(ctx: *Status, status: Status) void {
-            ctx.* = status;
-        }
-    }.callback);
-
-    while (true) {
-        if (response == .success) break;
-        mach.core.device.tick();
-    }
-
-    const result = resultBuffer.getConstMappedRange(f32, 0, input.len);
-    for (result.?) |v| {
-        std.debug.print("{d} ", .{v});
-    }
-    std.debug.print("\n", .{});
-    resultBuffer.unmap();
 }
 
 fn createRenderPipeline() *mach.gpu.RenderPipeline {
@@ -118,25 +62,6 @@ fn createRenderPipeline() *mach.gpu.RenderPipeline {
     const source = @embedFile("shader/shader.wgsl");
     const module = device.createShaderModuleWGSL("shader.wgsl", source);
     defer module.release();
-
-    // 顶点的布局
-    const vertexLayout = mach.gpu.VertexBufferLayout.init(.{
-        // 前面两个是坐标，后面三个是颜色
-        .array_stride = @sizeOf(f32) * 6,
-        .attributes = &.{
-            // 第一个是顶点坐标，偏移从 0 开始
-            .{ .format = .float32x3, .offset = 0, .shader_location = 0 },
-            // 第二个是颜色，偏移从 3 开始，shader_location 对应 WGSL 中的 location 位置
-            .{ .format = .float32x3, .offset = @sizeOf(f32) * 3, .shader_location = 1 },
-        },
-    });
-
-    // 顶点着色器状态
-    const vertex = mach.gpu.VertexState.init(.{
-        .module = module,
-        .entry_point = "vs_main",
-        .buffers = &.{vertexLayout},
-    });
 
     // 片段着色器状态
     const fragment = mach.gpu.FragmentState.init(.{
@@ -148,7 +73,7 @@ fn createRenderPipeline() *mach.gpu.RenderPipeline {
     // 创建渲染管线
     const descriptor = mach.gpu.RenderPipeline.Descriptor{
         .fragment = &fragment,
-        .vertex = &vertex,
+        .vertex = .{ .module = module, .entry_point = "vs_main" },
     };
     return device.createRenderPipeline(&descriptor);
 }
@@ -159,6 +84,45 @@ pub fn deinit(app: *App) void {
     _ = gpa.deinit();
 }
 
-pub fn update(_: *App) !bool {
-    return true;
+pub fn update(app: *App) !bool {
+    // 检查窗口是否需要关闭
+    var iterator = mach.core.pollEvents();
+    while (iterator.next()) |event| if (event == .close) return true;
+
+    // 清屏使用
+    const view = mach.core.swap_chain.getCurrentTextureView().?;
+    const colorAttachment = mach.gpu.RenderPassColorAttachment{
+        .view = view,
+        .clear_value = std.mem.zeroes(mach.gpu.Color),
+        .load_op = .clear,
+        .store_op = .store,
+    };
+
+    const renderPass = mach.gpu.RenderPassDescriptor.init(.{
+        .color_attachments = &.{colorAttachment},
+    });
+
+    // 命令编码器
+    const encoder = mach.core.device.createCommandEncoder(null);
+    const pass = encoder.beginRenderPass(&renderPass);
+
+    // 设置渲染管线
+    pass.setPipeline(app.renderPipeline);
+    pass.setBindGroup(0, app.bindGroup, null);
+    // 六个点，画两个三角形
+    pass.draw(6, 2, 0, 0);
+    pass.end();
+    pass.release();
+
+    var command = encoder.finish(null);
+    encoder.release();
+
+    // 提交命令
+    mach.core.queue.submit(&.{command});
+    command.release();
+    mach.core.swap_chain.present();
+    view.release();
+
+    // 不退出渲染循环
+    return false;
 }
