a4a7f8a646b55eb4a0f8444b22cf7989c0df9057
jiangbo <1006758852@qq.com>
2024-05-16 22:07:30 +0800

    提交游戏

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,6 @@ pub const App = @This();
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 renderPipeline: *mach.gpu.RenderPipeline = undefined,
-bindGroup: *mach.gpu.BindGroup = undefined,
 
 pub fn init(app: *App) !void {
     try mach.core.init(.{});
@@ -14,45 +13,9 @@ pub fn init(app: *App) !void {
     mach.core.setFrameRateLimit(30);
     mach.core.setInputFrequency(30);
 
-    const device = mach.core.device;
+    // const device = mach.core.device;
 
-    const red: u32 = 0xFF0000FF;
-    const yellow: u32 = 0xFFFF00FF;
-    const blue: u32 = 0x0000FFFF;
-
-    const textureData = [_]u32{
-        blue, red,    red,    red,    red, //
-        red,  yellow, yellow, yellow, red,
-        red,  yellow, red,    red,    red,
-        red,  yellow, yellow, red,    red,
-        red,  yellow, red,    red,    red,
-        red,  yellow, red,    red,    red,
-        red,  red,    red,    red,    red,
-    };
-
-    const size = mach.gpu.Extent3D{ .width = 5, .height = 7 };
-    const texture = device.createTexture(&.{
-        .label = "F texutre",
-        .size = .{ .width = size.width, .height = size.height },
-        .format = .rgba8_unorm,
-        .usage = .{ .texture_binding = true, .copy_dst = true, .render_attachment = true },
-    });
-
-    device.getQueue().writeTexture(&.{ .texture = texture }, &.{
-        .bytes_per_row = @sizeOf(u32) * 5,
-        .rows_per_image = 7,
-    }, &size, &textureData);
-
-    const sampler = device.createSampler(null);
     app.renderPipeline = createRenderPipeline();
-    app.bindGroup = device.createBindGroup(&.{
-        .layout = app.renderPipeline.getBindGroupLayout(0),
-        .entry_count = 2,
-        .entries = (&[2]mach.gpu.BindGroup.Entry{
-            mach.gpu.BindGroup.Entry.sampler(0, sampler),
-            mach.gpu.BindGroup.Entry.textureView(1, texture.createView(null)),
-        }),
-    });
 }
 
 fn createRenderPipeline() *mach.gpu.RenderPipeline {
@@ -89,39 +52,35 @@ pub fn update(app: *App) !bool {
     var iterator = mach.core.pollEvents();
     while (iterator.next()) |event| if (event == .close) return true;
 
-    // 清屏使用
     const view = mach.core.swap_chain.getCurrentTextureView().?;
-    const colorAttachment = mach.gpu.RenderPassColorAttachment{
-        .view = view,
-        .clear_value = std.mem.zeroes(mach.gpu.Color),
-        .load_op = .clear,
-        .store_op = .store,
-    };
+    defer view.release();
 
     const renderPass = mach.gpu.RenderPassDescriptor.init(.{
-        .color_attachments = &.{colorAttachment},
+        .color_attachments = &.{.{
+            .view = view,
+            .clear_value = std.mem.zeroes(mach.gpu.Color),
+            .load_op = .clear,
+            .store_op = .store,
+        }},
     });
 
     // 命令编码器
     const encoder = mach.core.device.createCommandEncoder(null);
+    defer encoder.release();
     const pass = encoder.beginRenderPass(&renderPass);
 
     // 设置渲染管线
     pass.setPipeline(app.renderPipeline);
-    pass.setBindGroup(0, app.bindGroup, null);
-    // 六个点，画两个三角形
-    pass.draw(6, 2, 0, 0);
+    pass.draw(3, 1, 0, 0);
     pass.end();
     pass.release();
 
     var command = encoder.finish(null);
-    encoder.release();
+    defer command.release();
 
     // 提交命令
     mach.core.queue.submit(&.{command});
-    command.release();
     mach.core.swap_chain.present();
-    view.release();
 
     // 不退出渲染循环
     return false;
--- src/shader/shader.wgsl
+++ src/shader/shader.wgsl
@@ -1,32 +1,16 @@
-struct VertexOutput {
-  @builtin(position) position: vec4f,
-  @location(0) texcoord: vec2f,
-};
-
 @vertex
-fn vs_main( @builtin(vertex_index) index : u32) -> VertexOutput {
-  let position = array(
-    // 1st triangle
-    vec2f( 0.0,  0.0),  // center
-    vec2f( 1.0,  0.0),  // right, center
-    vec2f( 0.0,  1.0),  // center, top
-
-    // 2st triangle
-    vec2f( 0.0,  1.0),  // center, top
-    vec2f( 1.0,  0.0),  // right, center
-    vec2f( 1.0,  1.0),  // right, top
-  );
-
-  var out: VertexOutput;
-  out.position = vec4f(position[index], 0.0, 1.0);
-  out.texcoord = position[index];
-  return out;
+fn vs_main(
+    @builtin(vertex_index) VertexIndex : u32
+) -> @builtin(position) vec4<f32> {
+    var pos = array<vec2<f32>, 3>(
+        vec2<f32>( 0.0,  0.5),
+        vec2<f32>(-0.5, -0.5),
+        vec2<f32>( 0.5, -0.5)
+    );
+    return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
 }
 
-@group(0) @binding(0) var ourSampler: sampler;
-@group(0) @binding(1) var ourTexture: texture_2d<f32>;
-
 @fragment
-fn fs_main(in: VertexOutput) -> @location(0) vec4f {
-  return textureSample(ourTexture, ourSampler, in.texcoord);
+fn fs_main() -> @location(0) vec4<f32> {
+    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
 }
