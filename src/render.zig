decbd673f06c39d1f2ef602b6e055e7aeb28cf16
jiangbo <1006758852@qq.com>
2024-05-19 16:02:32 +0800

    提交游戏

--- src/render.zig
+++ src/render.zig
@@ -31,12 +31,46 @@ pub const vertexData = [_]f32{
     70,  60,  0,
     30,  90,  0,
     70,  90,  0,
+
+    // left column back
+    0,   0,   30,
+    30,  0,   30,
+    0,   150, 30,
+    30,  150, 30,
+
+    // top rung back
+    30,  0,   30,
+    100, 0,   30,
+    30,  30,  30,
+    100, 30,  30,
+
+    // middle rung back
+    30,  60,  30,
+    70,  60,  30,
+    30,  90,  30,
+    70,  90,  30,
 };
 
 pub const indexData = [_]u32{
+    // front
     0, 1, 2, 2, 1, 3, // left column
     4, 5, 6, 6, 5, 7, // top run
     8, 9, 10, 10, 9, 11, // middle run
+
+    // back
+    12, 13, 14, 14, 13, 15, // left column back
+    16, 17, 18, 18, 17, 19, // top run back
+    20, 21, 22, 22, 21, 23, // middle run back
+    0, 5, 12, 12, 5, 17, // top
+    5, 7, 17, 17, 7, 19, // top rung right
+    6, 7, 18, 18, 7, 19, // top rung bottom
+    6, 8, 18, 18, 8, 20, // between top and middle rung
+    8, 9, 20, 20, 9, 21, // middle rung top
+    9, 11, 21, 21, 11, 23, // middle rung right
+    10, 11, 22, 22, 11, 23, // middle rung bottom
+    10, 3, 22, 22, 3, 15, // stem right
+    2, 3, 14, 14, 3, 15, // bottom
+    0, 2, 12, 12, 2, 14, // left
 };
 
 pub fn createRenderPipeline() RenderContext {
