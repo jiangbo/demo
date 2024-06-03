46af81720169e2918002f208873e5fac11dbcc34
JiangBo <1006758852@qq.com>
2024-06-03 15:47:17 +0800

    提交游戏

--- build.zig
+++ build.zig
@@ -5,36 +5,38 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const exe = b.addExecutable(.{
+        .name = "demo",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
     const mach_dep = b.dependency("mach", .{
         .target = target,
         .optimize = optimize,
-        .core = true,
     });
 
-    const zlm_dep = b.dependency("zlm", .{});
+    exe.root_module.addImport("mach", mach_dep.module("mach"));
+    @import("mach").link(mach_dep.builder, exe);
 
-    const app = try mach.CoreApp.init(b, mach_dep.builder, .{
-        .name = "demo",
-        .src = "src/main.zig",
-        .target = target,
-        .optimize = optimize,
-        .deps = &.{
-            .{ .name = "zlm", .module = zlm_dep.module("zlm") },
-        },
-    });
-
-    if (b.args) |args| app.run.addArgs(args);
-
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
     const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&app.run.step);
+    run_step.dependOn(&run_cmd.step);
 
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,124 +1,15 @@
-const std = @import("std");
-
 const mach = @import("mach");
-const render = @import("render.zig");
 
-const zm = @import("zmath.zig");
-const Vertex = @import("mesh.zig").Vertex;
-const vertices = @import("mesh.zig").vertices;
-
-pub const App = @This();
-
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const UniformBufferObject = struct {
-    mat: zm.Mat,
+// The global list of Mach modules registered for use in our application.
+pub const modules = .{
+    mach.Core,
+    @import("App.zig"),
 };
 
-timer: mach.core.Timer,
-timerBuffer: *mach.gpu.Buffer,
-bindGroup: *mach.gpu.BindGroup,
-renderContex: render.RenderContext,
+pub fn main() !void {
+    // Initialize mach.Core
+    try mach.core.initModule();
 
-pub fn init(app: *App) !void {
-    try mach.core.init(.{
-        .title = "学习 WebGPU",
-        .size = .{ .width = 960, .height = 640 },
-    });
-    // 设置帧率
-    mach.core.setFrameRateLimit(30);
-    mach.core.setInputFrequency(30);
-    const device = mach.core.device;
-
-    app.renderContext = render.createRenderPipeline();
-    app.timer = try mach.Timer.start();
-
-    app.timerBuffer = device.createBuffer(&.{
-        .usage = .{ .copy_dst = true, .uniform = true },
-        .size = @sizeOf(UniformBufferObject),
-    });
-
-    const bind_group = device.createBindGroup(
-        &mach.gpu.BindGroup.Descriptor.init(.{
-            .layout = app.renderContex.pipeline.getBindGroupLayout(0),
-            .entries = &.{
-                gpu.BindGroup.Entry.buffer(0, uniform_buffer, 0, @sizeOf(UniformBufferObject)),
-            },
-        }),
-    );
-}
-
-pub fn deinit(app: *App) void {
-    defer _ = gpa.deinit();
-    defer core.deinit();
-
-    app.vertex_buffer.release();
-    app.uniform_buffer.release();
-    app.bind_group.release();
-    app.pipeline.release();
-}
-
-pub fn update(app: *App) !bool {
-    var iter = core.pollEvents();
-    while (iter.next()) |event| {
-        switch (event) {
-            .key_press => |ev| {
-                if (ev.key == .space) return true;
-            },
-            .close => return true,
-            else => {},
-        }
-    }
-
-    const back_buffer_view = core.swap_chain.getCurrentTextureView().?;
-    const color_attachment = gpu.RenderPassColorAttachment{
-        .view = back_buffer_view,
-        .clear_value = std.mem.zeroes(gpu.Color),
-        .load_op = .clear,
-        .store_op = .store,
-    };
-
-    const queue = core.queue;
-    const encoder = core.device.createCommandEncoder(null);
-    const render_pass_info = gpu.RenderPassDescriptor.init(.{
-        .color_attachments = &.{color_attachment},
-    });
-
-    {
-        const time = app.timer.read();
-        const model = zm.mul(zm.rotationX(time * (std.math.pi / 2.0)), zm.rotationZ(time * (std.math.pi / 2.0)));
-        const view = zm.lookAtRh(
-            zm.Vec{ 0, 4, 2, 1 },
-            zm.Vec{ 0, 0, 0, 1 },
-            zm.Vec{ 0, 0, 1, 0 },
-        );
-        const proj = zm.perspectiveFovRh(
-            (std.math.pi / 4.0),
-            @as(f32, @floatFromInt(core.descriptor.width)) / @as(f32, @floatFromInt(core.descriptor.height)),
-            0.1,
-            10,
-        );
-        const mvp = zm.mul(zm.mul(model, view), proj);
-        const ubo = UniformBufferObject{
-            .mat = zm.transpose(mvp),
-        };
-        queue.writeBuffer(app.uniform_buffer, 0, &[_]UniformBufferObject{ubo});
-    }
-
-    const pass = encoder.beginRenderPass(&render_pass_info);
-    pass.setPipeline(app.pipeline);
-    pass.setVertexBuffer(0, app.vertex_buffer, 0, @sizeOf(Vertex) * vertices.len);
-    pass.setBindGroup(0, app.bind_group, &.{0});
-    pass.draw(vertices.len, 1, 0, 0);
-    pass.end();
-    pass.release();
-
-    var command = encoder.finish(null);
-    encoder.release();
-
-    queue.submit(&[_]*gpu.CommandBuffer{command});
-    command.release();
-    core.swap_chain.present();
-    back_buffer_view.release();
-
-    return false;
+    // Main loop
+    while (try mach.core.tick()) {}
 }
