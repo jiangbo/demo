2a67423e52f4612ae251e1175ffa4f819ba41f8a
jiangbo <1006758852@qq.com>
2024-07-03 00:18:21 +0800

    提交游戏

---
+++ src/win.zig
@@ -0,0 +1,82 @@
+const std = @import("std");
+const win32 = @import("win32");
+
+const ui = win32.ui.windows_and_messaging;
+const gdi = win32.graphics.gdi;
+const WINAPI = std.os.windows.WINAPI;
+
+pub const WIDTH: u32 = 2560;
+pub const HEIGHT: u32 = 1080;
+
+pub var instance: std.os.windows.HINSTANCE = undefined;
+pub var hander: win32.foundation.HWND = undefined;
+pub var rand: std.Random = undefined;
+
+pub fn mainWindowCallback(
+    window: win32.foundation.HWND,
+    message: u32,
+    wParam: win32.foundation.WPARAM,
+    lParam: win32.foundation.LPARAM,
+) callconv(WINAPI) win32.foundation.LRESULT {
+    switch (message) {
+        ui.WM_CREATE => {
+            std.log.info("WM_CREATE", .{});
+        },
+        ui.WM_DESTROY => {
+            std.log.info("WM_DESTROY", .{});
+            ui.PostQuitMessage(0);
+        },
+        else => return ui.DefWindowProc(window, message, wParam, lParam),
+    }
+    return 0;
+}
+
+const name = win32.zig.L("游戏编程大师");
+
+pub fn createWindow(h: std.os.windows.HINSTANCE) void {
+    std.log.info("wWinMain", .{});
+    var windowClass = std.mem.zeroes(ui.WNDCLASSEX);
+    const s = .{ .DBLCLKS = 1, .OWNDC = 1, .HREDRAW = 1, .VREDRAW = 1 };
+
+    windowClass.cbSize = @sizeOf(ui.WNDCLASSEX);
+    windowClass.style = s;
+    windowClass.lpszClassName = name;
+    windowClass.lpfnWndProc = mainWindowCallback;
+    windowClass.hInstance = h;
+    windowClass.hbrBackground = gdi.GetStockObject(gdi.BLACK_BRUSH);
+
+    if (ui.RegisterClassEx(&windowClass) == 0) win32Panic();
+
+    var style = ui.WS_OVERLAPPEDWINDOW;
+    style.VISIBLE = 1;
+    const window = ui.CreateWindowEx(ui.WS_EX_LEFT, name, name, style, 0, 0, //
+        @intCast(WIDTH), @intCast(HEIGHT), null, null, h, null);
+
+    instance = h;
+    hander = window orelse win32Panic();
+
+    const system = win32.system.system_information;
+    var prng = std.rand.DefaultPrng.init(system.GetTickCount64());
+    rand = prng.random();
+}
+
+pub fn update(gameUpdate: fn () void) void {
+    var message: ui.MSG = undefined;
+    while (true) {
+        if (ui.PeekMessage(&message, null, 0, 0, ui.PM_REMOVE) > 0) {
+            if (message.message == ui.WM_QUIT) break;
+            _ = ui.TranslateMessage(&message);
+            _ = ui.DispatchMessage(&message);
+        }
+
+        gameUpdate();
+    }
+
+    std.log.info("wWinMain end", .{});
+}
+
+pub fn win32Panic() noreturn {
+    const err = win32.foundation.GetLastError();
+    std.log.err("win32 painc code {}", .{@intFromEnum(err)});
+    @panic(@tagName(err));
+}
