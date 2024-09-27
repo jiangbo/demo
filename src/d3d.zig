ff1df2c984f0e570adc96272dea38ac4bb158b75
JiangBo <1006758852@qq.com>
2024-09-27 20:41:39 +0800

    提交游戏

---
+++ src/d3d.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+const win32 = @import("win32");
+
+const d3d9 = win32.graphics.direct3d9;
+const ui = win32.ui.windows_and_messaging;
+
+pub fn windowCallback(
+    window: win32.foundation.HWND,
+    message: u32,
+    wParam: win32.foundation.WPARAM,
+    lParam: win32.foundation.LPARAM,
+) callconv(std.os.windows.WINAPI) win32.foundation.LRESULT {
+    switch (message) {
+        ui.WM_CREATE => {
+            std.log.info("WM_CREATE", .{});
+        },
+        ui.WM_DESTROY => {
+            std.log.info("WM_DESTROY", .{});
+            ui.PostQuitMessage(0);
+        },
+        else => return ui.DefWindowProc(window, message, wParam, lParam),
+    }
+    return 0;
+}
+
+pub fn initDirectX(width: i32, height: i32) void {
+    const h = win32.system.library_loader.GetModuleHandle(null).?;
+    var windowClass = std.mem.zeroes(ui.WNDCLASSEX);
+
+    const className = win32.zig.L("TeachYourselfDirectX9");
+    windowClass.cbSize = @sizeOf(ui.WNDCLASSEX);
+    windowClass.style = .{ .HREDRAW = 1, .VREDRAW = 1 };
+    windowClass.lpszClassName = className;
+    windowClass.lpfnWndProc = windowCallback;
+    windowClass.hInstance = h;
+
+    win32Check(ui.RegisterClassEx(&windowClass));
+    var style = ui.WS_OVERLAPPEDWINDOW;
+    style.VISIBLE = 1;
+    const name = win32.zig.L("2D 游戏开发");
+    const window = ui.CreateWindowEx(ui.WS_EX_LEFT, className, name, //
+        style, 200, 200, width, height, null, null, h, null).?;
+
+    _ = window;
+
+    var message: ui.MSG = std.mem.zeroes(ui.MSG);
+    while (true) {
+        if (ui.PeekMessage(&message, null, 0, 0, ui.PM_REMOVE) > 0) {
+            if (message.message == ui.WM_QUIT) break;
+            _ = ui.TranslateMessage(&message);
+            _ = ui.DispatchMessage(&message);
+        }
+    }
+}
+
+pub fn win32Check(result: win32.foundation.HRESULT) void {
+    if (win32.zig.SUCCEEDED(result)) return;
+    const err = win32.foundation.GetLastError();
+    std.log.err("win32 panic code 0X{0X}", .{@intFromEnum(err)});
+    @panic(@tagName(err));
+}
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
+const win32 = @import("win32");
+const d3d = @import("d3d.zig");
+
+const d3d9 = win32.graphics.direct3d9;
+
+pub const UNICODE: bool = true;
 
 pub fn main() !void {
     std.log.debug("hello world", .{});
+
+    d3d.initDirectX(640, 480);
 }
