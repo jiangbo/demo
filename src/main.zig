de5dd9a2e8b87a4241f1e8b7a34b7c2932adfb24
jiangbo <1006758852@qq.com>
2025-02-11 22:45:16 +0800

    提交游戏

--- src/main.zig
+++ src/main.zig
@@ -25,38 +25,38 @@ export fn init() void {
     info.colors[0] = .{ .load_action = .CLEAR, .clear_value = clearColor };
 
     // 顶点数据
-    bind.vertex_buffers[0] = sk.gfx.makeBuffer(.{
-        .data = sk.gfx.asRange(&[_]f32{
-            // 位置           颜色
-            -1.0, -1.0, -1.0, 1.0, 0.0, 0.0, 1.0,
-            1.0,  -1.0, -1.0, 1.0, 0.0, 0.0, 1.0,
-            1.0,  1.0,  -1.0, 1.0, 0.0, 0.0, 1.0,
-            -1.0, 1.0,  -1.0, 1.0, 0.0, 0.0, 1.0,
+    bind.storage_buffers[shd.SBUF_ssbo] = sk.gfx.makeBuffer(.{
+        .type = .STORAGEBUFFER,
+        .data = sk.gfx.asRange(&[_]shd.SbVertex{
+            .{ .pos = .{ -1.0, -1.0, -1.0 }, .idx = 0, .uv = .{ 0.0, 0.0 } },
+            .{ .pos = .{ 1.0, -1.0, -1.0 }, .idx = 0, .uv = .{ 1.0, 0.0 } },
+            .{ .pos = .{ 1.0, 1.0, -1.0 }, .idx = 0, .uv = .{ 1.0, 1.0 } },
+            .{ .pos = .{ -1.0, 1.0, -1.0 }, .idx = 0, .uv = .{ 0.0, 1.0 } },
 
-            -1.0, -1.0, 1.0,  0.0, 1.0, 0.0, 1.0,
-            1.0,  -1.0, 1.0,  0.0, 1.0, 0.0, 1.0,
-            1.0,  1.0,  1.0,  0.0, 1.0, 0.0, 1.0,
-            -1.0, 1.0,  1.0,  0.0, 1.0, 0.0, 1.0,
+            .{ .pos = .{ -1.0, -1.0, 1.0 }, .idx = 1, .uv = .{ 0.0, 0.0 } },
+            .{ .pos = .{ 1.0, -1.0, 1.0 }, .idx = 1, .uv = .{ 1.0, 0.0 } },
+            .{ .pos = .{ 1.0, 1.0, 1.0 }, .idx = 1, .uv = .{ 1.0, 1.0 } },
+            .{ .pos = .{ -1.0, 1.0, 1.0 }, .idx = 1, .uv = .{ 0.0, 1.0 } },
 
-            -1.0, -1.0, -1.0, 0.0, 0.0, 1.0, 1.0,
-            -1.0, 1.0,  -1.0, 0.0, 0.0, 1.0, 1.0,
-            -1.0, 1.0,  1.0,  0.0, 0.0, 1.0, 1.0,
-            -1.0, -1.0, 1.0,  0.0, 0.0, 1.0, 1.0,
+            .{ .pos = .{ -1.0, -1.0, -1.0 }, .idx = 2, .uv = .{ 0.0, 0.0 } },
+            .{ .pos = .{ -1.0, 1.0, -1.0 }, .idx = 2, .uv = .{ 1.0, 0.0 } },
+            .{ .pos = .{ -1.0, 1.0, 1.0 }, .idx = 2, .uv = .{ 1.0, 1.0 } },
+            .{ .pos = .{ -1.0, -1.0, 1.0 }, .idx = 2, .uv = .{ 0.0, 1.0 } },
 
-            1.0,  -1.0, -1.0, 1.0, 0.5, 0.0, 1.0,
-            1.0,  1.0,  -1.0, 1.0, 0.5, 0.0, 1.0,
-            1.0,  1.0,  1.0,  1.0, 0.5, 0.0, 1.0,
-            1.0,  -1.0, 1.0,  1.0, 0.5, 0.0, 1.0,
+            .{ .pos = .{ 1.0, -1.0, -1.0 }, .idx = 3, .uv = .{ 0.0, 0.0 } },
+            .{ .pos = .{ 1.0, 1.0, -1.0 }, .idx = 3, .uv = .{ 1.0, 0.0 } },
+            .{ .pos = .{ 1.0, 1.0, 1.0 }, .idx = 3, .uv = .{ 1.0, 1.0 } },
+            .{ .pos = .{ 1.0, -1.0, 1.0 }, .idx = 3, .uv = .{ 0.0, 1.0 } },
 
-            -1.0, -1.0, -1.0, 0.0, 0.5, 1.0, 1.0,
-            -1.0, -1.0, 1.0,  0.0, 0.5, 1.0, 1.0,
-            1.0,  -1.0, 1.0,  0.0, 0.5, 1.0, 1.0,
-            1.0,  -1.0, -1.0, 0.0, 0.5, 1.0, 1.0,
+            .{ .pos = .{ -1.0, -1.0, -1.0 }, .idx = 4, .uv = .{ 0.0, 0.0 } },
+            .{ .pos = .{ -1.0, -1.0, 1.0 }, .idx = 4, .uv = .{ 1.0, 0.0 } },
+            .{ .pos = .{ 1.0, -1.0, 1.0 }, .idx = 4, .uv = .{ 1.0, 1.0 } },
+            .{ .pos = .{ 1.0, -1.0, -1.0 }, .idx = 4, .uv = .{ 0.0, 1.0 } },
 
-            -1.0, 1.0,  -1.0, 1.0, 0.0, 0.5, 1.0,
-            -1.0, 1.0,  1.0,  1.0, 0.0, 0.5, 1.0,
-            1.0,  1.0,  1.0,  1.0, 0.0, 0.5, 1.0,
-            1.0,  1.0,  -1.0, 1.0, 0.0, 0.5, 1.0,
+            .{ .pos = .{ -1.0, 1.0, -1.0 }, .idx = 5, .uv = .{ 0.0, 0.0 } },
+            .{ .pos = .{ -1.0, 1.0, 1.0 }, .idx = 5, .uv = .{ 1.0, 0.0 } },
+            .{ .pos = .{ 1.0, 1.0, 1.0 }, .idx = 5, .uv = .{ 1.0, 1.0 } },
+            .{ .pos = .{ 1.0, 1.0, -1.0 }, .idx = 5, .uv = .{ 0.0, 1.0 } },
         }),
     });
 
@@ -73,14 +73,45 @@ export fn init() void {
         }),
     });
 
+    // 颜色索引
+    bind.storage_buffers[shd.SBUF_colors] = sk.gfx.makeBuffer(.{
+        .type = .STORAGEBUFFER,
+        .data = sk.gfx.asRange(&[_]shd.SbColor{
+            .{ .color = .{ 1.0, 0.0, 0.0, 1.0 } },
+            .{ .color = .{ 0.0, 1.0, 0.0, 1.0 } },
+            .{ .color = .{ 0.0, 0.0, 1.0, 1.0 } },
+            .{ .color = .{ 0.5, 0.0, 1.0, 1.0 } },
+            .{ .color = .{ 0.0, 0.5, 1.0, 1.0 } },
+            .{ .color = .{ 1.0, 0.5, 0.0, 1.0 } },
+        }),
+    });
+
+    const pixels: [4][4]u8 = .{
+        .{ 0xFF, 0xCC, 0x88, 0x44 },
+        .{ 0xCC, 0x88, 0x44, 0xFF },
+        .{ 0x88, 0x44, 0xFF, 0xCC },
+        .{ 0x44, 0xFF, 0xCC, 0x88 },
+    };
+
+    bind.images[shd.IMG_tex] = sk.gfx.makeImage(.{
+        .width = 4,
+        .height = 4,
+        .pixel_format = .R8,
+        .data = init: {
+            var data = sk.gfx.ImageData{};
+            data.subimage[0][0] = sk.gfx.asRange(&pixels);
+            break :init data;
+        },
+    });
+
+    // ...and a matching sampler
+    bind.samplers[shd.SMP_smp] = sk.gfx.makeSampler(.{
+        .min_filter = .NEAREST,
+        .mag_filter = .NEAREST,
+    });
+
     pipeline = sk.gfx.makePipeline(.{
         .shader = sk.gfx.makeShader(shd.testShaderDesc(sk.gfx.queryBackend())),
-        .layout = init: {
-            var l = sk.gfx.VertexLayoutState{};
-            l.attrs[shd.ATTR_test_position].format = .FLOAT3;
-            l.attrs[shd.ATTR_test_color0].format = .FLOAT4;
-            break :init l;
-        },
         .index_type = .UINT16,
         .depth = .{
             .compare = .LESS_EQUAL,
@@ -120,32 +151,15 @@ export fn frame() void {
 }
 
 fn computeParams() shd.VsParams {
-    // const rxm = zm.rotationX(rx);
-    // const rym = zm.rotationY(ry);
-    // const model = zm.mul(rxm, rym);
+    const rxm = zm.rotationX(std.math.degreesToRadians(rx));
+    const rym = zm.rotationY(std.math.degreesToRadians(ry));
+    const model = zm.mul(rym, rxm);
 
-    // const aspect = sk.app.widthf() / sk.app.heightf();
-    // const p = zm.perspectiveFovLh(60.0, aspect, 0.01, 10);
-
-    // return shd.VsParams{ .vp = zm.mul(zm.mul(model, view), p) };
-
-    const xm = mat4.rotate(rx, .{ .x = 1.0, .y = 0.0, .z = 0.0 });
-    const ym = mat4.rotate(ry, .{ .x = 0.0, .y = 1.0, .z = 0.0 });
-    const m = mat4.mul(xm, ym);
-    std.log.info("m: {}", .{m});
     const aspect = sk.app.widthf() / sk.app.heightf();
-    const proj = mat4.persp(60.0, aspect, 0.01, 10.0);
+    const radians = std.math.degreesToRadians(60.0);
+    const proj = zm.perspectiveFovRh(radians, aspect, 0.01, 10.0);
 
-    return shd.VsParams{ .vp = zm.mul(zm.mul(convert(m), view), convert(proj)) };
-}
-
-fn convert(mvp: mat4) zm.Mat {
-    return zm.Mat{
-        zm.f32x4(mvp.m[0][0], mvp.m[0][1], mvp.m[0][2], mvp.m[0][3]),
-        zm.f32x4(mvp.m[1][0], mvp.m[1][1], mvp.m[1][2], mvp.m[1][3]),
-        zm.f32x4(mvp.m[2][0], mvp.m[2][1], mvp.m[2][2], mvp.m[2][3]),
-        zm.f32x4(mvp.m[3][0], mvp.m[3][1], mvp.m[3][2], mvp.m[3][3]),
-    };
+    return shd.VsParams{ .vp = zm.mul(zm.mul(model, view), proj) };
 }
 
 export fn cleanup() void {
@@ -159,7 +173,6 @@ pub fn main() void {
         .window_title = "学习 sokol",
         .logger = .{ .func = sk.log.func },
         .win32_console_attach = true,
-        .sample_count = 4,
         .init_cb = init,
         .frame_cb = frame,
         .cleanup_cb = cleanup,
--- src/shader/test.glsl
+++ src/shader/test.glsl
@@ -6,23 +6,43 @@ layout(binding=0) uniform vs_params {
     mat4 vp;
 };
 
-in vec4 position;
-in vec4 color0;
+struct sb_vertex {
+    vec3 pos;
+    uint idx;
+    vec2 uv;
+};
 
-out vec4 color;
+layout(binding=0) readonly buffer ssbo {
+    sb_vertex vtx[];
+};
+
+out vec3 uv_idx;
 
 void main() {
-    gl_Position = vp * position;
-    color = color0;
+    gl_Position = vp * vec4(vtx[gl_VertexIndex].pos, 1.0);
+    uv_idx = vec3(vtx[gl_VertexIndex].uv, float(vtx[gl_VertexIndex].idx) + 0.5);
 }
 @end
 
 @fs fs
-in vec4 color;
+layout(binding=0) uniform texture2D tex;
+layout(binding=0) uniform sampler smp;
+
+struct sb_color {
+    vec4 color;
+};
+
+layout(binding=1) readonly buffer colors {
+    sb_color clr[];
+};
+
+in vec3 uv_idx;
 out vec4 frag_color;
 
 void main() {
-     frag_color = color;
+    uint idx = uint(uv_idx.z);
+    vec2 uv = uv_idx.xy;
+    frag_color = vec4(texture(sampler2D(tex,smp), uv).xxx, 1.0) * clr[idx].color;
 }
 @end
 
