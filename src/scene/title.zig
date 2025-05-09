1ee76736117048612b3895517e5680ee9bc4ab96
JiangBo <1006758852@qq.com>
2025-05-09 12:26:19 +0800

    提交游戏

--- src/scene/title.zig
+++ src/scene/title.zig
@@ -4,16 +4,10 @@ const window = @import("../window.zig");
 const gfx = @import("../graphics.zig");
 const scene = @import("../scene.zig");
 
-var background1: gfx.Texture = undefined;
-
-var start1: gfx.Texture = undefined;
-var start2: gfx.Texture = undefined;
-
-var load1: gfx.Texture = undefined;
-var load2: gfx.Texture = undefined;
-
-var exit1: gfx.Texture = undefined;
-var exit2: gfx.Texture = undefined;
+var backgrounds: [3]gfx.Texture = undefined;
+var currentBackground: u8 = 0;
+var timer: window.Timer = .init(5);
+var logo: gfx.Texture = undefined;
 
 const Button = struct {
     normal: gfx.Texture,
@@ -24,7 +18,11 @@ var buttons: [3]Button = undefined;
 var currentButton: u8 = 0;
 
 pub fn init() void {
-    background1 = gfx.loadTexture("assets/T_bg1.png", .init(800, 600));
+    backgrounds[0] = gfx.loadTexture("assets/T_bg1.png", .init(800, 600));
+    backgrounds[1] = gfx.loadTexture("assets/T_bg2.png", .init(800, 600));
+    backgrounds[2] = gfx.loadTexture("assets/T_bg3.png", .init(800, 600));
+
+    logo = gfx.loadTexture("assets/T_logo.png", .init(274, 102));
 
     buttons[0] = .{
         .normal = gfx.loadTexture("assets/T_start_1.png", .init(142, 36)),
@@ -52,7 +50,11 @@ pub fn exit() void {
 }
 
 pub fn update(delta: f32) void {
-    _ = delta;
+    if (timer.isFinishedAfterUpdate(delta)) {
+        currentBackground += 1;
+        currentBackground %= backgrounds.len;
+        timer.reset();
+    }
 
     if (window.isAnyKeyRelease(&.{ .W, .UP })) currentButton -|= 1;
     if (window.isAnyKeyRelease(&.{ .S, .DOWN })) currentButton += 1;
@@ -72,7 +74,9 @@ pub fn render() void {
     gfx.beginDraw();
     defer gfx.endDraw();
 
-    gfx.draw(background1, .zero);
+    gfx.draw(backgrounds[currentBackground], .zero);
+
+    gfx.draw(logo, .init(260, 80));
 
     for (buttons, 0..) |button, index| {
         const offsetY: f32 = @floatFromInt(350 + index * 50);
