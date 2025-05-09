cfd15a6ca4b7a2d277ae59d9281cf4d7d6e625ce
JiangBo <1006758852@qq.com>
2025-05-09 15:29:23 +0800

    提交游戏

--- src/scene/title.zig
+++ src/scene/title.zig
@@ -9,13 +9,24 @@ var currentBackground: u8 = 0;
 var timer: window.Timer = .init(5);
 var logo: gfx.Texture = undefined;
 
-const Button = struct {
-    normal: gfx.Texture,
-    hover: gfx.Texture,
+const Button = struct { normal: gfx.Texture, hover: gfx.Texture };
+
+var menuButtons: [3]Button = undefined;
+var currentButton: u8 = 0;
+
+const Popup = struct {
+    background: gfx.Texture,
+    buttons: [2]Button = undefined,
+    current: u8 = 0,
+
+    pub fn shadow(self: *Popup) void {
+        displayPopup = false;
+        self.current = 0;
+    }
 };
 
-var buttons: [3]Button = undefined;
-var currentButton: u8 = 0;
+var popup: Popup = undefined;
+var displayPopup: bool = false;
 
 pub fn init() void {
     backgrounds[0] = gfx.loadTexture("assets/T_bg1.png", .init(800, 600));
@@ -24,19 +35,31 @@ pub fn init() void {
 
     logo = gfx.loadTexture("assets/T_logo.png", .init(274, 102));
 
-    buttons[0] = .{
-        .normal = gfx.loadTexture("assets/T_start_1.png", .init(142, 36)),
-        .hover = gfx.loadTexture("assets/T_start_2.png", .init(142, 36)),
+    const size = gfx.Vector.init(142, 36);
+    menuButtons[0] = .{
+        .normal = gfx.loadTexture("assets/T_start_1.png", size),
+        .hover = gfx.loadTexture("assets/T_start_2.png", size),
     };
 
-    buttons[1] = .{
-        .normal = gfx.loadTexture("assets/T_load_1.png", .init(142, 36)),
-        .hover = gfx.loadTexture("assets/T_load_2.png", .init(142, 36)),
+    menuButtons[1] = .{
+        .normal = gfx.loadTexture("assets/T_load_1.png", size),
+        .hover = gfx.loadTexture("assets/T_load_2.png", size),
     };
 
-    buttons[2] = .{
-        .normal = gfx.loadTexture("assets/T_exit_1.png", .init(142, 36)),
-        .hover = gfx.loadTexture("assets/T_exit_2.png", .init(142, 36)),
+    menuButtons[2] = .{
+        .normal = gfx.loadTexture("assets/T_exit_1.png", size),
+        .hover = gfx.loadTexture("assets/T_exit_2.png", size),
+    };
+
+    const bg = gfx.loadTexture("assets/confirm_bg.png", .init(227, 155));
+    popup = Popup{ .background = bg };
+    popup.buttons[0] = .{
+        .normal = gfx.loadTexture("assets/confirm_yes_1.png", size),
+        .hover = gfx.loadTexture("assets/confirm_yes_2.png", size),
+    };
+    popup.buttons[1] = .{
+        .normal = gfx.loadTexture("assets/confirm_no_1.png", size),
+        .hover = gfx.loadTexture("assets/confirm_no_2.png", size),
     };
 }
 
@@ -56,16 +79,30 @@ pub fn update(delta: f32) void {
         timer.reset();
     }
 
-    if (window.isAnyKeyRelease(&.{ .W, .UP })) currentButton -|= 1;
-    if (window.isAnyKeyRelease(&.{ .S, .DOWN })) currentButton += 1;
-    currentButton = @min(currentButton, buttons.len - 1);
+    if (displayPopup) {
+        if (window.isAnyKeyRelease(&.{ .W, .UP })) popup.current -|= 1;
+        if (window.isAnyKeyRelease(&.{ .S, .DOWN })) popup.current += 1;
+        popup.current = @min(popup.current, popup.buttons.len - 1);
 
-    if (window.isAnyKeyRelease(&.{ .ENTER, .SPACE })) {
-        switch (currentButton) {
-            0 => scene.changeScene(),
-            1 => std.log.info("load game", .{}),
-            2 => window.exit(),
-            else => unreachable,
+        if (window.isAnyKeyRelease(&.{ .ENTER, .SPACE })) {
+            switch (popup.current) {
+                0 => window.exit(),
+                1 => popup.shadow(),
+                else => unreachable,
+            }
+        }
+    } else {
+        if (window.isAnyKeyRelease(&.{ .W, .UP })) currentButton -|= 1;
+        if (window.isAnyKeyRelease(&.{ .S, .DOWN })) currentButton += 1;
+        currentButton = @min(currentButton, menuButtons.len - 1);
+
+        if (window.isAnyKeyRelease(&.{ .ENTER, .SPACE })) {
+            switch (currentButton) {
+                0 => scene.changeScene(),
+                1 => std.log.info("load game", .{}),
+                2 => displayPopup = true,
+                else => unreachable,
+            }
         }
     }
 }
@@ -75,15 +112,23 @@ pub fn render() void {
     defer gfx.endDraw();
 
     gfx.draw(backgrounds[currentBackground], .zero);
-
     gfx.draw(logo, .init(260, 80));
 
+    if (displayPopup) {
+        gfx.draw(popup.background, .init(283, 250));
+        renderButtons(&popup.buttons, popup.current, 305);
+    } else {
+        renderButtons(&menuButtons, currentButton, 350);
+    }
+}
+
+fn renderButtons(buttons: []Button, current: u8, y: f32) void {
     for (buttons, 0..) |button, index| {
-        const offsetY: f32 = @floatFromInt(350 + index * 50);
-        if (currentButton == index) {
-            gfx.draw(button.hover, .init(325, offsetY));
+        const offsetY: f32 = @floatFromInt(index * 50);
+        if (current == index) {
+            gfx.draw(button.hover, .init(325, y + offsetY));
         } else {
-            gfx.draw(button.normal, .init(325, offsetY));
+            gfx.draw(button.normal, .init(325, y + offsetY));
         }
     }
 }
