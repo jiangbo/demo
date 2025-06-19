f8f3ed913cf2d0edf8321c46f8444c2687c4d9e7
jiangbo <1006758852@qq.com>
2025-06-19 23:01:35 +0800

    提交游戏

--- src/engine/root.zig
+++ src/engine/root.zig
@@ -2,3 +2,4 @@ pub const window = @import("window.zig");
 pub const audio = @import("audio.zig");
 pub const gfx = @import("graphics.zig");
 pub const camera = @import("camera.zig");
+pub const math = @import("math.zig");
--- src/world.zig
+++ src/world.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 const window = @import("zhu").window;
 const gfx = @import("zhu").gfx;
 const camera = @import("zhu").camera;
+const math = @import("zhu").math;
 
 var playerTexture: gfx.Texture = undefined;
 var mapTexture: gfx.Texture = undefined;
@@ -16,6 +17,13 @@ const map: Map = @import("zon/map.zon");
 
 var tiles: [500]camera.Vertex = undefined;
 var tileIndex: usize = 0;
+
+const FrameAnimation = gfx.FixedFrameAnimation(3, 0.1);
+const Animation = std.EnumArray(math.FourDirection, FrameAnimation);
+
+var playerAnimation: Animation = undefined;
+var playerDirection: math.FourDirection = .up;
+var playerPosition: math.Vector = .init(180, 164);
 
 pub fn init() void {
     playerTexture = gfx.loadTexture("assets/pic/player.png", .init(96, 192));
@@ -45,6 +53,8 @@ pub fn init() void {
         tileIndex += 1;
     }
 
+    playerAnimation = createAnimation("assets/pic/player.png");
+
     // window.playMusic("assets/voc/back.ogg");
 }
 
@@ -52,6 +62,24 @@ fn getAreaFromIndex(index: usize) gfx.Rectangle {
     const row: f32 = @floatFromInt(index / 20);
     const col: f32 = @floatFromInt(index % 20);
     return .init(.init(col * 32, row * 32), .init(32, 32));
+}
+
+fn createAnimation(path: [:0]const u8) Animation {
+    var animation = Animation.initUndefined();
+
+    const texture = gfx.loadTexture(path, .init(96, 192));
+    var tex = texture.subTexture(.init(.zero, .init(96, 48)));
+    animation.set(.down, FrameAnimation.init(tex));
+
+    tex = texture.subTexture(.init(.init(0, 48), .init(96, 48)));
+    animation.set(.left, FrameAnimation.init(tex));
+
+    tex = texture.subTexture(.init(.init(0, 96), .init(96, 48)));
+    animation.set(.right, FrameAnimation.init(tex));
+
+    tex = texture.subTexture(.init(.init(0, 144), .init(96, 48)));
+    animation.set(.up, FrameAnimation.init(tex));
+    return animation;
 }
 
 pub fn update(delta: f32) void {
@@ -64,5 +92,7 @@ pub fn exit() void {}
 
 pub fn render() void {
     camera.drawVertex(mapTexture, tiles[0..tileIndex]);
-    // camera.draw(playerTexture, .init(100, 100));
+
+    const animation = playerAnimation.get(playerDirection);
+    camera.draw(animation.currentTexture(), playerPosition);
 }
