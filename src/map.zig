b81d2a2dad306656974d47642674944c28a7abab
JiangBo <1006758852@qq.com>
2025-06-20 19:30:39 +0800

    提交游戏

--- src/map.zig
+++ src/map.zig
@@ -9,8 +9,8 @@ var texture: gfx.Texture = undefined;
 const Map = struct {
     width: u16,
     height: u16,
-    ground: []const u16,
-    prop: []const u16,
+    ground1: []const u16,
+    ground2: []const u16,
     object: []const u16,
 };
 
@@ -19,31 +19,41 @@ const map: Map = @import("zon/map.zon");
 var vertexBuffer: [500]camera.Vertex = undefined;
 var vertexIndex: usize = 0;
 
+var objectOffset: usize = 0;
+var objectArray: [1000]u16 = undefined;
+
 pub fn init() void {
     texture = gfx.loadTexture("assets/pic/maps.png", .init(640, 1536));
 
-    buildVertexBuffer(map.ground);
-    buildVertexBuffer(map.prop);
+    buildVertexBuffer(map.ground1);
+    buildVertexBuffer(map.ground2);
+    objectOffset = vertexIndex;
+
+    @memcpy(objectArray[0..map.object.len], map.object);
+    buildObjectBuffer();
 }
 
 fn buildVertexBuffer(tiles: []const u16) void {
     for (tiles, 0..) |tileIndex, index| {
-        const x: u32 = 4;
-        const y: u32 = 4;
-        if (index == x + y * 20) {
-            std.log.info("index: {}, tileIndex: {}", .{ index, tileIndex });
-        }
-
-        if (tileIndex == 0) continue;
-
-        const tile = texture.subTexture(getAreaFromIndex(tileIndex));
-        vertexBuffer[vertexIndex] = .{
-            .position = getAreaFromIndex(index).min,
-            .size = .init(32, 32),
-            .texture = tile.area.toVector4(),
-        };
-        vertexIndex += 1;
+        if (tileIndex != 0) appendVertex(tileIndex, index);
     }
+}
+
+fn buildObjectBuffer() void {
+    vertexIndex = objectOffset;
+    for (objectArray[0..map.object.len], 0..) |tileIndex, index| {
+        if (tileIndex > 256) appendVertex(tileIndex, index);
+    }
+}
+
+fn appendVertex(tileIndex: usize, index: usize) void {
+    const tile = texture.subTexture(getAreaFromIndex(tileIndex));
+    vertexBuffer[vertexIndex] = .{
+        .position = getAreaFromIndex(index).min,
+        .size = .init(32, 32),
+        .texture = tile.area.toVector4(),
+    };
+    vertexIndex += 1;
 }
 
 fn getAreaFromIndex(index: usize) gfx.Rectangle {
