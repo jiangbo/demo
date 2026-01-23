12bbe66fd78f7275b00ca35b50b3f2d3cb75c6cf
JiangBo <1006758852@qq.com>
2026-01-23 12:20:45 +0800

    键盘控制角色

--- sunny/player.zig
+++ sunny/player.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
 const zhu = @import("zhu");
 
+const moveForce = 200; // 移动力
+const factor = 0.85; // 减速因子
+const maxSpeed = 120; // 最大速度
 const size: zhu.Vector2 = .xy(32, 32);
 var image: zhu.graphics.Image = undefined;
 
+var velocity: zhu.Vector2 = .zero;
 var position: zhu.Vector2 = undefined;
 
 pub fn init(pos: zhu.Vector2) void {
@@ -13,9 +17,23 @@ pub fn init(pos: zhu.Vector2) void {
 }
 
 pub fn update(delta: f32) void {
-    _ = delta;
+    if (zhu.window.isKeyDown(.A)) {
+        if (velocity.x > 0) velocity.x = 0;
+        velocity.x -= moveForce * delta;
+    } else if (zhu.window.isKeyDown(.D)) {
+        if (velocity.x < 0) velocity.x = 0;
+        velocity.x += moveForce * delta;
+    } else {
+        // 没有按的时候，减少速度
+        velocity.x *= factor;
+    }
+
+    velocity.x = std.math.clamp(velocity.x, -maxSpeed, maxSpeed);
+    position = position.add(velocity.scale(delta));
 }
 
 pub fn draw() void {
-    zhu.batch.drawImage(image, position, .{});
+    zhu.batch.drawImage(image, position, .{
+        .flipX = velocity.x < 0,
+    });
 }
