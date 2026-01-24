6c028bcc239b1aaef873cbc74c14117e1d13ccad
jiangbo <1006758852@qq.com>
2026-01-24 18:31:00 +0800

    实现樱桃动画

--- sunny/object.zig
+++ sunny/object.zig
@@ -6,8 +6,11 @@ const batch = zhu.batch;
 const map = @import("map.zig");
 const getImage = zhu.assets.getImage;
 
-const gemFrames = zhu.graphics.loopFramesX(5, .xy(15, 13), 0.2);
+const gemFrames = zhu.graphics.framesX(5, .xy(15, 13), 0.2);
+const cherryFrames = zhu.graphics.loopFramesX(5, .xy(21, 21), 0.2);
+
 var gemAnimation: zhu.graphics.FrameAnimation = undefined;
+var cherryAnimation: zhu.graphics.FrameAnimation = undefined;
 
 var items: []map.Object = undefined;
 
@@ -16,16 +19,21 @@ pub fn init(objects: []map.Object) void {
 
     const gemImage = getImage(@intFromEnum(map.ObjectEnum.gem));
     gemAnimation = .init(gemImage, &gemFrames);
+
+    const cherryImage = getImage(@intFromEnum(map.ObjectEnum.cherry));
+    cherryAnimation = .init(cherryImage, &cherryFrames);
 }
 
 pub fn update(delta: f32) void {
     gemAnimation.loopUpdate(delta);
+    cherryAnimation.loopUpdate(delta);
 }
 
 pub fn draw() void {
     for (items) |item| {
         const image: ?zhu.graphics.Image = switch (item.type) {
             .gem => gemAnimation.currentImage(),
+            .cherry => cherryAnimation.currentImage(),
             else => null,
         };
 
