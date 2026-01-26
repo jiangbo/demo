90230e92dcf22f6297681b41ae018fd54ee0ceb2
JiangBo <1006758852@qq.com>
2026-01-26 11:12:12 +0800

    实现老鹰的动画

--- sunny/object.zig
+++ sunny/object.zig
@@ -9,10 +9,12 @@ const getImage = zhu.assets.getImage;
 const gemFrames = zhu.graphics.framesX(5, .xy(15, 13), 0.2);
 const cherryFrames = zhu.graphics.loopFramesX(5, .xy(21, 21), 0.2);
 const opossumFrames = zhu.graphics.framesX(6, .xy(36, 28), 0.1);
+const eagleFrames = zhu.graphics.framesX(4, .xy(40, 41), 0.15);
 
 var gemAnimation: zhu.graphics.FrameAnimation = undefined;
 var cherryAnimation: zhu.graphics.FrameAnimation = undefined;
 var opossumAnimation: zhu.graphics.FrameAnimation = undefined;
+var eagleAnimation: zhu.graphics.FrameAnimation = undefined;
 
 var items: []map.Object = undefined;
 
@@ -27,12 +29,16 @@ pub fn init(objects: []map.Object) void {
 
     const opossumImage = getImage(@intFromEnum(map.ObjectEnum.opossum));
     opossumAnimation = .init(opossumImage, &opossumFrames);
+
+    const eagleImage = getImage(@intFromEnum(map.ObjectEnum.eagle));
+    eagleAnimation = .init(eagleImage, &eagleFrames);
 }
 
 pub fn update(delta: f32) void {
     gemAnimation.loopUpdate(delta);
     cherryAnimation.loopUpdate(delta);
     opossumAnimation.loopUpdate(delta);
+    eagleAnimation.loopUpdate(delta);
 }
 
 pub fn draw() void {
@@ -41,6 +47,7 @@ pub fn draw() void {
             .gem => gemAnimation.currentImage(),
             .cherry => cherryAnimation.currentImage(),
             .opossum => opossumAnimation.currentImage(),
+            .eagle => eagleAnimation.currentImage(),
             else => null,
         };
 
