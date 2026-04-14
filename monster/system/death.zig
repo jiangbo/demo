82d525b21c263750b39d76ee2bb3b876247001d3
JiangBo <1006758852@qq.com>
2026-04-14 12:08:28 +0800

    修复死亡敌人不消失

--- monster/system/death.zig
+++ monster/system/death.zig
@@ -6,31 +6,30 @@ const spawn = @import("../spawn.zig");
 
 /// 处理死亡实体
 pub fn update(reg: *zhu.ecs.Registry, _: f32) void {
-    var ghostView = reg.view(.{com.Ghost});
-    while (ghostView.next()) |entity| {
-        std.log.info("处理幽灵实体：{}", .{entity});
+    var deadView = reg.reverseView(.{com.Dead});
+    while (deadView.next()) |entity| {
+        std.log.info("处理死亡实体：{}", .{entity});
 
-        if (ghostView.tryGet(entity, com.motion.BlockBy)) |blockBy| {
+        if (!deadView.has(entity, com.Enemy)) {
+            reg.destroyEntity(deadView.toEntity(entity));
+            continue;
+        }
+
+        if (deadView.tryGet(entity, com.motion.BlockBy)) |blockBy| {
             // 死亡实体被阻挡了，释放阻挡锁定
             if (reg.tryGetPtr(blockBy.v, com.motion.Blocker)) |blocker| {
                 blocker.current -|= 1;
             }
         }
 
-        reg.removeExcept(ghostView.toEntity(entity), .{
-            com.Ghost,
+        reg.removeExcept(deadView.toEntity(entity), .{
             com.Position,
             com.Animation,
             com.Sprite,
         });
-        ghostView.add(entity, com.animation.Play{
+        deadView.add(entity, com.Ghost{});
+        deadView.add(entity, com.animation.Play{
             .index = @intFromEnum(com.StateEnum.damage),
         });
-    }
-
-    var deadView = reg.reverseView(.{com.Dead});
-    while (deadView.next()) |entity| {
-        std.log.info("删除死亡实体：{}", .{entity});
-        reg.destroyEntity(deadView.toEntity(entity));
     }
 }
--- monster/system/health.zig
+++ monster/system/health.zig
@@ -37,9 +37,7 @@ pub fn update(reg: *zhu.ecs.Registry, _: f32) void {
 
         view.add(target.index, com.attack.Injured{}); // 目标受伤了
         if (stats.health <= 0) {
-            if (view.has(target.index, com.Enemy)) {
-                view.add(target.index, com.Ghost{}); // 敌人需要播放死亡动画
-            } else view.add(target.index, com.Dead{}); // 目标死了
+            view.add(target.index, com.Dead{}); // 目标死了
             std.log.debug("实体: {} 杀死了目标: {}", .{ entity, target.index });
         }
     }
