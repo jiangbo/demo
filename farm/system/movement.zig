e9bee983a692d94a6c14dafe3e771e5138445db7
JiangBo <1006758852@qq.com>
2026-05-22 15:56:19 +0800

    增加测试

--- farm/system/movement.zig
+++ farm/system/movement.zig
@@ -50,15 +50,8 @@ test "有 Collider 的实体会被 solid 格子阻挡" {
     zhu.assets.initCaches(std.testing.allocator);
     defer zhu.assets.deinit();
     const count = map.data.width * map.data.height;
-    map.cells = zhu.assets.oomAlloc(map.Cell, count);
     map.solids = zhu.assets.oomAlloc(bool, count);
-    defer {
-        zhu.assets.free(map.cells);
-        zhu.assets.free(map.solids);
-        map.cells = &.{};
-        map.solids = &.{};
-    }
-    @memset(map.cells, .{});
+    defer zhu.assets.free(map.solids);
     @memset(map.solids, false);
 
     // 标记 tile (2,2) 为 solid（世界坐标 32~48, 32~48）
@@ -79,4 +72,55 @@ test "有 Collider 的实体会被 solid 格子阻挡" {
     // 向左移动会碰到 solid 格子，x 应被阻挡
     const position = world.get(entity, Position).?;
     try std.testing.expectEqual(@as(f32, 56), position.x);
+}
+
+test "有 Collider 的实体会被 solid 格子垂直阻挡" {
+    zhu.assets.initCaches(std.testing.allocator);
+    defer zhu.assets.deinit();
+    const count = map.data.width * map.data.height;
+    map.solids = zhu.assets.oomAlloc(bool, count);
+    defer zhu.assets.free(map.solids);
+    @memset(map.solids, false);
+
+    // 标记 tile (2,2) 为 solid（世界坐标 32~48, 32~48）
+    map.solids[map.data.worldToTileIndex(.xy(40, 40)).?] = true;
+
+    var world = zhu.ecs.World.init(std.testing.allocator);
+    defer world.deinit();
+
+    const entity = world.createEntity();
+    world.add(entity, Position.xy(40, 60));
+    world.add(entity, Velocity{ .value = .xy(0, -20) });
+    world.add(entity, Collider{ .size = .xy(10, 6), .offset = .xy(-5, -6) });
+
+    update(&world, 1.0);
+
+    const position = world.get(entity, Position).?;
+    try std.testing.expectEqual(@as(f32, 60), position.y);
+}
+
+test "斜向撞墙时未碰撞轴仍会滑动" {
+    zhu.assets.initCaches(std.testing.allocator);
+    defer zhu.assets.deinit();
+    const count = map.data.width * map.data.height;
+    map.solids = zhu.assets.oomAlloc(bool, count);
+    defer zhu.assets.free(map.solids);
+    @memset(map.solids, false);
+
+    // 标记 tile (2,2) 为 solid（世界坐标 32~48, 32~48）
+    map.solids[map.data.worldToTileIndex(.xy(40, 40)).?] = true;
+
+    var world = zhu.ecs.World.init(std.testing.allocator);
+    defer world.deinit();
+
+    const entity = world.createEntity();
+    world.add(entity, Position.xy(56, 40));
+    world.add(entity, Velocity{ .value = .xy(-20, 5) });
+    world.add(entity, Collider{ .size = .xy(10, 6), .offset = .xy(-5, -6) });
+
+    update(&world, 1.0);
+
+    const position = world.get(entity, Position).?;
+    try std.testing.expectEqual(@as(f32, 56), position.x);
+    try std.testing.expectEqual(@as(f32, 45), position.y);
 }
