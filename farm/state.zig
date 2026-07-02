fc6530b9aa771cfb27bdc14c671f94913da76f46
jiangbo <1006758852@qq.com>
2026-07-02 22:23:11 +0800

    修改名称

--- farm/state.zig
+++ farm/state.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 const zhu = @import("zhu");
 
-pub const Clock = @import("state/Clock.zig");
-pub const Notice = @import("state/Notice.zig");
-pub const Maps = @import("state/Maps.zig");
+pub const Clock = @import("resource/Clock.zig");
+pub const Notice = @import("resource/Notice.zig");
+pub const Maps = @import("resource/Maps.zig");
 
 pub const Session = struct {
     clock: Clock = .{},
