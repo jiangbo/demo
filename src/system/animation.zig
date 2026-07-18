7d14a62fb7c2be4f34f4d2a95ad76088726ec676
jiangbo <1006758852@qq.com>
2026-07-18 19:41:25 +0800

    渲染系统

--- src/system/animation.zig
+++ src/system/animation.zig
@@ -1,19 +1,21 @@
 const ecs = @import("ecs");
 
 const component = @import("../component.zig");
-const Actor = component.Actor;
 const Animation = component.Animation;
+const Facing = component.Facing;
+const WantMove = component.WantMove;
 
 pub fn update(world: *ecs.World, delta: f32) void {
-    var query = world.query(.{ Actor, Animation });
+    var query = world.query(.{ Facing, Animation });
     while (query.next()) |entity| {
-        const actor = query.get(entity, Actor);
+        const facing = query.get(entity, Facing);
         const animation = query.getPtr(entity, Animation);
-        const sourceIndex: u8 = @intFromEnum(actor.facing);
+        const sourceIndex: u8 = @intFromEnum(facing);
 
         if (animation.sourceIndex != sourceIndex) {
             animation.play(sourceIndex);
         }
+        if (!world.has(entity, WantMove)) continue;
         _ = animation.update(delta);
     }
 }
--- src/system/render.zig
+++ src/system/render.zig
@@ -2,16 +2,26 @@ const ecs = @import("ecs");
 const zhu = @import("zhu");
 
 const component = @import("../component.zig");
-const Actor = component.Actor;
 const Animation = component.Animation;
+const Position = component.Position;
+const RenderOffset = component.RenderOffset;
 
 pub fn draw(world: *ecs.World) void {
-    var query = world.query(.{ Actor, Animation });
+    world.sort(Position, lessY);
+
+    var query = world.queryBy(Position, .{Animation}, .{});
     while (query.next()) |entity| {
-        const actor = query.get(entity, Actor);
+        var position = query.get(entity, Position);
         const animation = query.get(entity, Animation);
-        zhu.batch.drawImage(animation.subImage(), actor.position, .{
+        if (world.get(entity, RenderOffset)) |offset| {
+            position = position.add(offset.value);
+        }
+        zhu.batch.drawImage(animation.subImage(), position, .{
             .anchor = .xy(0.5, 1),
         });
     }
+}
+
+fn lessY(lhs: Position, rhs: Position) bool {
+    return lhs.y < rhs.y;
 }
