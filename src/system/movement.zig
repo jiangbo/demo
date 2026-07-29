57fba47fabcc3299a7d02626f1169ad2c2334e78
jiangbo <1006758852@qq.com>
2026-07-29 22:29:11 +0800

    修改

--- src/system/movement.zig
+++ src/system/movement.zig
@@ -10,37 +10,19 @@ const Position = component.Position;
 const Speed = component.Speed;
 const WantMove = component.WantMove;
 
-// 移动系统本帧计算出的位移。
-const Move = struct { offset: zhu.Vector2 };
-
 pub fn update(world: *ecs.World, delta: f32) void {
-    prepareMove(world, delta);
-    updateMove(world);
-    world.clear(Move);
-}
-
-fn prepareMove(world: *ecs.World, delta: f32) void {
-    // 根据移动意图计算本帧位移。
-    var query = world.query(.{ Position, Speed, WantMove });
-    while (query.next()) |entity| {
-        const speed = query.get(entity, Speed);
-        const wantMove = query.get(entity, WantMove);
-        const offset = wantMove.value.scale(speed.value * delta);
-        world.add(entity, Move{ .offset = offset });
-    }
-}
-
-fn updateMove(world: *ecs.World) void {
     // 检查碰撞后应用真正允许到达的位置。
-    var query = world.query(.{ Position, Collider, Move });
+    var query = world.query(.{ Position, Collider, Speed, WantMove });
     move: while (query.next()) |entity| {
         const position = query.getPtr(entity, Position);
         const collider = query.get(entity, Collider);
-        const move = query.get(entity, Move);
+        const speed = query.get(entity, Speed);
+        const wantMove = query.get(entity, WantMove);
+        const offset = wantMove.value.scale(speed.value * delta);
 
         // 先将本帧位移限制在可通过的地图瓦片内。
         const area = collider.move(position.*);
-        const moved = map.walk(area, move.offset);
+        const moved = map.walk(area, offset);
 
         // 与任一实体重叠时，放弃本帧移动。
         var others = world.query(.{ Position, Collider });
